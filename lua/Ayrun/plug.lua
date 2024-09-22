54275998fbcf6cfb408302e7f20631f94dc0bba6
itzAyrun <ayrun3412@gmail.com>
2024-09-22 20:36:13 +0530

    Install nvim-cmp and remove gruvbox, coc.nvim

--- lua/Ayrun/plug.lua
+++ lua/Ayrun/plug.lua
@@ -1,15 +1,23 @@
 vim.cmd ([[
   call plug#begin()
+  Plug 'neovim/nvim-lspconfig'
+  Plug 'hrsh7th/cmp-nvim-lsp'
+  Plug 'hrsh7th/cmp-buffer'
+  Plug 'hrsh7th/cmp-path'
+  Plug 'hrsh7th/cmp-cmdline'
+  Plug 'hrsh7th/nvim-cmp'
+  Plug 'hrsh7th/cmp-nvim-lsp-signature-help'
+  " For luasnip users.
+  Plug 'L3MON4D3/LuaSnip'
+  Plug 'saadparwaiz1/cmp_luasnip'
   Plug 'sainnhe/gruvbox-material'
   Plug 'vyfor/cord.nvim', { 'do': './build' }
-  Plug 'ellisonleao/gruvbox.nvim'
   Plug 'nvim-lua/plenary.nvim'
   Plug 'nvim-telescope/telescope-file-browser.nvim'
   Plug 'nvim-telescope/telescope.nvim', { 'branch': '0.1.x' }
   Plug 'nvim-lualine/lualine.nvim'
   Plug 'https://github.com/tpope/vim-commentary'
   Plug 'nvim-tree/nvim-web-devicons'
-  Plug 'https://github.com/neoclide/coc.nvim' 
   Plug 'https://github.com/windwp/nvim-autopairs'
   Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
   Plug 'iamcco/markdown-preview.nvim', { 'do': 'cd app && npx --yes yarn install' }
