fe8e8f23b4c6a95c80d71539991e980ea18f0123
itzAyrun <ayrun3412@gmail.com>
2024-10-04 17:28:31 +0530

    Update setup code for gruvbox-material

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,24 +4,16 @@ vim.opt.background = "dark"
 -- Enable true color support
 vim.opt.termguicolors = true
 
--- neomodern colorscheme settings
-require('neomodern').setup({
-	style = "roseprime",
-	transparent = true,
-	term_colors = true,
-	code_style = {
-		comments = "none",
-		headings = "bold",  -- markdown headings
-	},
-	ui = {
-		colored_doctrings = true,
-	},
-	telescope = "borderless",
-	lualine = {
-		bold = true,
-		plain = false,
-	},
-})
-
--- Set colorscheme to neomodern
-require('neomodern').load()
+-- gruvbox material setup
+vim.cmd([[
+	" Set contrast
+	let g:gruvbox_material_background = 'hard'
+	" For better performance
+	let g:gruvbox_material_better_preformance = 1
+	" Disable italic comment
+	let g:gruvbox_material_disable_italic_comment = 1
+	" Enable transparent background
+	let g:gruvbox_material_transparent_background = 1
+	" Set colorscheme
+	colorscheme gruvbox-material
+]])
