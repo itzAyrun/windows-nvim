bbca71460dfa3ef40d2ad7f6392f37dc4c8535c3
itzAyrun <ayrun3412@gmail.com>
2024-10-06 21:10:39 +0530

    refactor: Add keymaps for resizing windows, switching windows, e.t.c

--- lua/Ayrun/remap.lua
+++ lua/Ayrun/remap.lua
@@ -1,5 +1,24 @@
+-- Define options for each keymap
+local opts = { noremap = true, silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
 -- Set leader key to space
 vim.g.mapleader = " "
 
 -- Clear search highlight
-vim.api.nvim_set_keymap('n', '<leader>a', ':nohlsearch<CR>', { noremap = true, silent = true })
+keymap('n', '<leader>a', ':nohlsearch<CR>', opts)
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<leader>q", "<C-w>w", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
